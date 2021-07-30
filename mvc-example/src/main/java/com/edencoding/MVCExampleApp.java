5517be0cbb5d8924b925df5bf42b35d68c29843d
Ed Eden-Rump <ed.edenrump@gmail.com>
2021-07-30 06:35:47 +0100

    add icon to Stage

--- mvc-example/src/main/java/com/edencoding/MVCExampleApp.java
+++ mvc-example/src/main/java/com/edencoding/MVCExampleApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MVCExampleApp extends Application {
@@ -12,6 +13,7 @@ public class MVCExampleApp extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainView.fxml"));
         primaryStage.setTitle("MVC Example App");
+        primaryStage.getIcons().add(new Image(getClass().getResource("/img/EdenCodingIcon.png").toExternalForm()));
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
